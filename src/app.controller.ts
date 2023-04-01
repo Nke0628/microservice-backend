4be2ed1c79ab92886a5bdf55e3b1d04433519721
Nke0628 <ooo_diva@yahoo.co.jp>
2023-04-01 09:35:56 +0900

    gRPC導入

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
 import { AppService } from './app.service';
+import {
+  AppServiceController,
+  SamepleData,
+  SampleDataById,
+} from './proto/sample';
 
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+export class AppController implements AppServiceController {
+  @GrpcMethod('AppService')
+  findOne(
+    request: SampleDataById,
+  ): SamepleData | Promise<SamepleData> | Observable<SamepleData> {
+    const itmes = [
+      { id: 1, name: 'john' },
+      { id: 2, name: 'Doe' },
+    ] as SamepleData[];
+    const filterdItem = itmes.filter((item) => item.id === request.id);
+    return filterdItem.length > 0 ? filterdItem[0] : ({} as SamepleData);
   }
 }
