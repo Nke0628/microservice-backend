5914e056928020d1d55e35b4eb5970b50924690c
Nke0628 <ooo_diva@yahoo.co.jp>
2023-04-03 21:15:23 +0900

    Prisma導入

---
+++ prisma/migrations/20230402114027_v1/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE `BusinessTerm` (
+    `id` INTEGER NOT NULL,
+    `term_name` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
